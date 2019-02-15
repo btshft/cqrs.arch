eeb57b1f453f0afbb2881721a4d27455e6578daf
Поляков Игорь <bitshift.it@icloud.com>
2019-02-15 20:34:24 +0300

    Missing files

---
+++ Cqrs.ComponentRegistrar/ApplicationRegistrar.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using Cqrs.AppServices.Application.CommandHandlers;
+using Cqrs.Domain.Data;
+using Cqrs.Infrastructure.Data;
+using Cqrs.Infrastructure.Dispatcher;
+using Cqrs.Infrastructure.Mapper;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Cqrs.ComponentRegistrar
+{
+    /// <summary>
+    /// Компонент регистрации зависимостей приложения.
+    /// </summary>
+    public class ApplicationRegistrar
+    {
+        public static void RegisterComponents(IServiceCollection services)
+        {
+            RegisterMediator(services);
+            RegisterDataComponents(services);
+        }
+
+        private static void RegisterMediator(IServiceCollection services)
+        {
+            services.AddMediatR(typeof(CreateApplicationCommandHandler));
+            services.AddScoped<IMediatorDispatcher, MediatorDispatcher>();
+        }
+
+        private static void RegisterDataComponents(IServiceCollection services)
+        {
+            services.AddScoped(typeof(IRepository<>), typeof(EntityFrameworkRepository<>));
+            services.AddScoped<IDataSession, EntityFrameworkDataSession>();
+            services.AddScoped<IDataSessionFactory, EntityFrameworkDataSessionFactory>();
+            services.AddScoped<ApplicationDbContext>();
+        }
+
+        private static void RegisterTypeMapper(IServiceCollection services)
+        {
+            services.AddAutoMapper();
+            services.AddScoped<ITypeMapper, TypeMapper>();
+        }
+    }
+}
---
+++ Cqrs.WebApi/Startup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cqrs.ComponentRegistrar;
+using Cqrs.WebApi.Api;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Cqrs.WebApi
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public IHostingEnvironment Environment { get; }
+        
+        public Startup(IConfiguration configuration, IHostingEnvironment environment)
+        {
+            Configuration = configuration;
+            Environment = environment;
+        }
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOptions();         
+            services.AddFeatures(Configuration);
+            
+            ApplicationRegistrar.RegisterComponents(services);
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseFeatures(Configuration);
+        }
+    }
+}
